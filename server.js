8bbe1600fecc9956ca3e9ed85ca2dfc1a36a18d6
Deep871xd <deepakoraon11454@gmail.com>
2022-07-08 18:15:00 +0530

    initial author routes

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000
-const expressLayout = require("express-ejs-layouts")
-const indexRouter = require("./routes/index")
-const authorRouter = require("./routes/authors")
-const bookRouter = require("./routes/books")
-app.set("view engine", "ejs")
-app.set("views", __dirname+"/views");
-app.set("layout", "layouts/layout" )
+const port = process.env.PORT || 3000;
+const expressLayout = require("express-ejs-layouts");
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
+const bodyParser = require("body-parser");
 
-app.use(express.static("public"))
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+
+app.use(express.static("public"));
 app.use(expressLayout);
-
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
-
-  
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL);
@@ -26,9 +26,7 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
-
-
 app.listen(port, () => {
-    console.log(`Connected with the port ${port}`);
+  console.log(`Connected with the port ${port}`);
+});
    
-}) 
