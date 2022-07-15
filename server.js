e37b415411e57a7c58886629032f776351168355
Deep871xd <deepakoraon11454@gmail.com>
2022-07-16 01:59:16 +0530

    final setup

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const expressLayout = require("express-ejs-layouts");
+const methodOverride = require("method-override");
+
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
 const bookRouter = require("./routes/books");
-const bodyParser = require("body-parser");
+
+
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+
+/* 
+no need to import and use bodyParser now as it is built into express now
+   so it would be like
+   const express = require("express");
+   app.use(express.urlencoded({ limit: "10mb", extended: false }));
+*/
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -14,7 +26,8 @@ app.set("layout", "layouts/layout");
 
 app.use(express.static("public"));
 app.use(expressLayout);
-app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ limit: "10mb", extended: false }));
 
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
@@ -29,4 +42,3 @@ db.once("open", () => console.log("Connected to Mongoose"));
 app.listen(port, () => {
   console.log(`Connected with the port ${port}`);
 });
-   
